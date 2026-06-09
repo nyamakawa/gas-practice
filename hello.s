7d6eeeb0cd6a1b9a84754a06323faa775782e273
nyamakawa <nyamakawa@gmail.com>
2026-06-09 10:35:04 +0900

    use labels

--- hello.s
+++ hello.s
@@ -55,20 +55,23 @@
 
         .text
 _start:
-        # write(1, message, 13)
-        mov     $1, %rax                # system call 1 is write
-        mov     $1, %rdi                # file handle 1 is stdout
-        mov     $message, %rsi          # address of string to output
-        mov     $13, %rdx               # number of bytes
-        syscall                         # invoke operating system to do the write
+        call hello
+        call getc
+        call putc
 
+        jmp exit
+
+hello:
         syscall3 1, $1, $message2, $14     # write(1, message2, 14) by macro
-
-        # getc and putc
+        ret
+getc:
         syscall3 0, $0, $buff, $2     # read(0, buff, 2)
+        ret
+putc:
         syscall3 1, $1, $buff, $2     # write(1, buff, 2)
+        ret
 
-        # exit(0)
+exit:
         mov     $60, %rax               # system call 60 is exit
         xor     %rdi, %rdi              # we want return code 0
         syscall                         # invoke operating system to exit
